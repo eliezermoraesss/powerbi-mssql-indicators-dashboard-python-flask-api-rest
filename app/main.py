7dad0cd5ac070ce08a30a00ce09fef4a49fd9b2e
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2024-07-12 11:34:35 -0300

    feat: added BackgroundScheduler to run automatic tasks

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify
 from app import create_app
 from app.queries import get_all_indicators, get_all_totvs_indicators
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
 
 app = create_app()
 
@@ -12,10 +14,24 @@ def home():
 def all_indicators():
     data = get_all_indicators()
     return jsonify(data)
+
 @app.route('/totvs-indicators', methods=['GET'])
 def all_totvs_indicators():
     data = get_all_totvs_indicators()
     return jsonify(data)
 
+def run_task():
+    try:
+        response = requests.get('http://127.0.0.1:5000/indicators')
+        print(response.json())
+    except requests.exceptions.ConnectionError as e:
+        print(f"Erro de conexão: {e}")
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(run_task, 'interval', seconds=5)
+    scheduler.start()
+
+    app.run(host='0.0.0.0', port=5000, use_reloader=False)
+
+
