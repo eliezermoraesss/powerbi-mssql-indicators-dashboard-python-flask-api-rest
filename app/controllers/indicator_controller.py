927b2f25ec7f03c7229f54aa76485f28daaecb98
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2024-07-15 10:11:06 -0300

    refactor: good practices had been applied in get_all_indicators() function

--- app/controllers/indicator_controller.py
+++ app/controllers/indicator_controller.py
@@ -7,26 +7,43 @@ def get_all_indicators():
     cod_qps = [row[0] for row in cod_qps]
 
     data = {}
+    indicators = {
+        "baseline": "vl_proj_all_prod",
+        "desconsiderar": "vl_proj_prod_cancel",
+        "indice_mudanca": "vl_proj_modify_perc",
+        "projeto_liberado": "vl_proj_released",
+        "projeto_pronto": "vl_proj_finished",
+        "em_ajuste": "vl_proj_adjusted",
+        "quant_pi_proj": "vl_proj_pi",
+        "quant_mp_proj": "vl_proj_mp",
+        "indice_pcp": "vl_pcp_perc",
+        "indice_producao": "vl_product_perc",
+        "indice_compra": "vl_compras_perc",
+        "indice_recebimento": "vl_mat_received_perc"
+    }
+
     for cod_qp in cod_qps:
-        data[cod_qp] = {
-            "baseline": get_indicator_value("TOP 1 vl_proj_all_prod", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "desconsiderar": get_indicator_value("TOP 1 vl_proj_prod_cancel", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "indice_mudanca": get_indicator_value("TOP 1 vl_proj_modify_perc", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "projeto_liberado": get_indicator_value("TOP 1 vl_proj_released", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "projeto_pronto": get_indicator_value("TOP 1 vl_proj_finished", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "em_ajuste": get_indicator_value("TOP 1 vl_proj_adjusted", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "quant_pi_proj": get_indicator_value("TOP 1 vl_proj_pi", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "quant_mp_proj": get_indicator_value("TOP 1 vl_proj_mp", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "op_aberta_fechada": get_indicator_value("COUNT(*)", "SC2010", f"C2_ZZNUMQP LIKE '%{cod_qp}' AND C2_DATRF = '       ' AND D_E_L_E_T_ <> '*'"),
-            "indice_pcp": get_indicator_value("TOP 1 vl_pcp_perc", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "op_fechada": get_indicator_value("COUNT(*)", "SC2010", f"C2_ZZNUMQP LIKE '%{cod_qp}' AND C2_DATRF <> '       ' AND D_E_L_E_T_ <> '*'"),
-            "indice_producao": get_indicator_value("TOP 1 vl_product_perc", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "sc_aberta_fechada": get_indicator_value("COUNT(*)", "SC1010", f"C1_ZZNUMQP LIKE '%{cod_qp}' AND C1_PEDIDO = '      ' AND D_E_L_E_T_ <> '*'"),
-            "pc_total": get_indicator_value("COUNT(*)", "SC7010", f"C7_ZZNUMQP LIKE '%{cod_qp}' AND D_E_L_E_T_ <> '*'"),
-            "indice_compra": get_indicator_value("TOP 1 vl_compras_perc", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC"),
-            "mat_entregue": get_indicator_value("COUNT(*)", "SC7010", f"C7_ZZNUMQP LIKE '%{cod_qp}' AND C7_ENCER = 'E' AND D_E_L_E_T_ <> '*'"),
-            "indice_recebimento": get_indicator_value("TOP 1 vl_mat_received_perc", "enaplic_management.dbo.tb_dashboard_indicators", f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC")
-        }
+        data[cod_qp] = {}
+        for key, indicator in indicators.items():
+            data[cod_qp][key] = get_indicator_value(f"TOP 1 {indicator}",
+                                                    "enaplic_management.dbo.tb_dashboard_indicators",
+                                                    f"cod_qp LIKE '%{cod_qp}' ORDER BY id DESC")
+
+        # Adiciona os valores que dependem de contagens específicas
+        data[cod_qp]["op_aberta_fechada"] = get_indicator_value("COUNT(*)", "SC2010",
+                                                                f"C2_ZZNUMQP LIKE '%{cod_qp}' AND C2_DATRF = '       ' AND D_E_L_E_T_ <> '*'")
+
+        data[cod_qp]["op_fechada"] = get_indicator_value("COUNT(*)", "SC2010",
+                                                         f"C2_ZZNUMQP LIKE '%{cod_qp}' AND C2_DATRF <> '       ' AND D_E_L_E_T_ <> '*'")
+
+        data[cod_qp]["sc_aberta_fechada"] = get_indicator_value("COUNT(*)", "SC1010",
+                                                                f"C1_ZZNUMQP LIKE '%{cod_qp}' AND C1_PEDIDO = '      ' AND D_E_L_E_T_ <> '*'")
+
+        data[cod_qp]["pc_total"] = get_indicator_value("COUNT(*)", "SC7010",
+                                                       f"C7_ZZNUMQP LIKE '%{cod_qp}' AND D_E_L_E_T_ <> '*'")
+
+        data[cod_qp]["mat_entregue"] = get_indicator_value("COUNT(*)", "SC7010",
+                                                           f"C7_ZZNUMQP LIKE '%{cod_qp}' AND C7_ENCER = 'E' AND D_E_L_E_T_ <> '*'")
 
     data = percentage_indicators_calculate(data)
 
@@ -55,22 +72,34 @@ def get_indicator_value(select_clause, table_name,where_clause):
     return result[0] if result else 0
 
 def percentage_indicators_calculate(data):
-
+    # Verificações para evitar divisões por zero
     for cod_qp, values in data.items():
-        indice_producao = (values['op_fechada'] / values['op_aberta_fechada']) * 100
-        indice_compra = (values['pc_total'] / values['sc_aberta_fechada']) * 100
-        indice_recebimento = (values['mat_entregue'] / values['pc_total']) * 100
 
-        data[cod_qp]['indice_producao'] = indice_producao
-        data[cod_qp]['indice_compra'] = indice_compra
-        data[cod_qp]['indice_recebimento'] = indice_recebimento
+        if values['op_aberta_fechada'] != 0:
+            indice_producao = (values['op_fechada'] / values['op_aberta_fechada']) * 100
+        else:
+            indice_producao = 0
+
+        if values['sc_aberta_fechada'] != 0:
+            indice_compra = (values['pc_total'] / values['sc_aberta_fechada']) * 100
+        else:
+            indice_compra = 0
+
+        if values['pc_total'] != 0:
+            indice_recebimento = (values['mat_entregue'] / values['pc_total']) * 100
+        else:
+            indice_recebimento = 0
+
+        data[cod_qp]['indice_producao'] = round(indice_producao, 2)
+        data[cod_qp]['indice_compra'] = round(indice_compra, 2)
+        data[cod_qp]['indice_recebimento'] = round(indice_recebimento, 2)
 
     return data
 
 def save_totvs_indicator():
-    dict_data = get_all_indicators()
+    data = get_all_indicators()
 
-    for cod_qp, values in dict_data.items():
+    for cod_qp, values in data.items():
         insert_query = text("""
         INSERT INTO 
             enaplic_management.dbo.tb_dashboard_indicators 
@@ -87,4 +116,3 @@ def save_totvs_indicator():
             'vl_mat_received': values['mat_entregue'],
         })
         db.session.commit()
-
--- app/main.py
+++ app/main.py
@@ -20,21 +20,21 @@ def all_totvs_indicators():
     data = get_all_totvs_indicators()
     return jsonify(data)
 
-@app.route('/refresh-totvs-indicators', methods=['GET'])
+@app.route('/refresh-totvs-indicators', methods=['POST'])
 def save_totvs_indicators():
     save_totvs_indicator()
     return f"Atualização de indicadores realizada com sucesso!", 200
 
 def scheduled_task_insert_totvs_indicators():
     try:
-        requests.get('http://localhost:5000/refresh-totvs-indicators')
+        requests.post('http://localhost:5000/refresh-totvs-indicators')
         print("Atualização de indicadores realizada com sucesso!")
     except requests.exceptions.ConnectionError as e:
         print(f"Erro de conexão: {e}")
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(scheduled_task_insert_totvs_indicators, 'interval', seconds=5)
+    scheduler.add_job(scheduled_task_insert_totvs_indicators, 'interval', minutes=30)
     scheduler.start()
 
     app.run(host='0.0.0.0', port=5000, use_reloader=False)
